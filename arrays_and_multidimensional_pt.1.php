7f89b733ee57c394cd133465202877f7ff37a71c
lolgamebot <rageandfury2@gmail.com>
2026-04-24 11:08:35 +0800

    UI changes
    Changed Color schemes

--- arrays_and_multidimensional_pt.1.php
+++ arrays_and_multidimensional_pt.1.php
@@ -1,77 +1,53 @@
 <?php
-$output = null;
-
-$user = [
-    'name' => 'Juan',
-    'email' => 'juan@gmail.com',
+$result = null;
+$currentUser = [
+    'name' => 'Basty',
+    'email' => 'basty@gmail.com',
     'password' => '123456',
     'hobbies' => ['Video Games', 'Tennis'],
 ];
 
-// Examples
-// $output = $user['name'];
-// $output = $user['hobbies'][0];
-
-
-$fruits = [
+$fruitList = [
     ['Apple', 'Red'],
     ['Orange', 'Orange'],
     ['Banana', 'Yellow']
 ];
 
-// Examples
-// $fruits[] = ['Grapes', 'Purple'];
-// $output = $fruits[3][0];
-
-
-$users = [
-    ['name' => 'Juan',   'email' => 'juan@gmail.com',   'password' => '123456'],
-    ['name' => 'Harold', 'email' => 'kakong@gmail.com', 'password' => 'pogi123'],
-    ['name' => 'Mackie', 'email' => 'makivero@gmail.com', 'password' => 'rizzmahal']
+$userList = [
+    ['name' => 'Basty',   'email' => 'basty@gmail.com',   'password' => '123456'],
+    ['name' => 'Nateman', 'email' => 'nateman@gmail.com', 'password' => 'nathane'],
+    ['name' => 'Portugal', 'email' => 'Speed@gmail.com', 'password' => 'iwshowspeed']
 ];
 
-// Example
-// $output = $users[1]['email'];
-
-
-
-$tempUser = [
-    ['name' => 'Maria', 'email' => 'maria@gmail.com', 'password' => 'mariaSKIBIDI']
+$tempUserEntry = [
+    ['name' => 'Erlinda', 'email' => 'Erlinda@gmail.com', 'password' => 'Erlinda']
 ];
-
-unset($tempUser[2]);
-
-$output = count($tempUser);
+unset($tempUserEntry[2]);
+$result = count($tempUserEntry);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>PHP Arrays and Multidimensional</title>
 </head>
-
-<body class="bg-gray-100">
-    <header class="bg-blue-500 text-white p-4">
+<body class="bg-emerald-50">
+    <header class="bg-emerald-600 text-white p-4">
         <div class="container mx-auto">
             <h1 class="text-3xl font-semibold">PHP Arrays</h1>
         </div>
     </header>
-
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-            <p class="text-xl font-semibold mb-4">
-                Output: <?= $output ?>
+            <p class="text-xl font-semibold mb-4 text-emerald-700">
+                Output: <?= $result ?>
             </p>
-
-            <pre class="bg-gray-100 p-3 rounded">
-<?php print_r($tempUser); ?>
+            <pre class="bg-emerald-100 p-3 rounded text-emerald-900">
+<?php print_r($tempUserEntry); ?>
             </pre>
         </div>
     </div>
 </body>
-
 </html>
